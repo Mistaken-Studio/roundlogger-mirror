dbbb1423ad8958e72c80e7eb312694e64a37206d
Xname <xnameserver@o2.pl>
2022-04-14 20:37:06 +0200

    useless negation

--- RoundLogger/RoundLogHandler.cs
+++ RoundLogger/RoundLogHandler.cs
@@ -224,7 +224,7 @@ namespace Mistaken.RoundLogger
 
         private void Player_IntercomSpeaking(Exiled.Events.EventArgs.IntercomSpeakingEventArgs ev)
         {
-            if (!Round.IsStarted)
+            if (Round.IsStarted)
                 RLogger.Log("GAME EVENT", "INTERCOM", $"{this.PTS(ev.Player)} is using intercom ({(ev.IsAllowed ? "allowed" : "disallowed")})");
         }
 
